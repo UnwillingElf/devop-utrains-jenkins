b574abb8a9ea676e74d818fe80366d44c9bc4404
UnwillingElf <register_matthew@yahoo.com>
2025-03-20 14:34:50 -0400

    comments

--- FULL_INFRA_WITH_DOCKER/main.tf
+++ FULL_INFRA_WITH_DOCKER/main.tf
@@ -66,8 +66,8 @@ resource "aws_security_group" "web-sg" {
   }
   ingress {
     description = "Jenkins Port"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 8081
+    to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
